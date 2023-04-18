7594224e92f6b8daa1c6ea0589a0e3e664c754d7
Tiago Gomes <tidgomes@gmail.com>
2023-04-18 09:12:18 -0300

    use literal types

--- app.ts
+++ app.ts
@@ -1,14 +1,24 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
   let result
-  if (typeof input1 === 'number' && typeof input2 === 'number')
-    result = input1 + input2
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  )
+    result = +input1 + +input2
   else result = input1.toString() + input2.toString()
 
   return result
 }
 
-const combinedNumbers = combine(2, 3)
+const combinedNumbers = combine(2, 3, 'as-number')
 console.log(combinedNumbers)
 
-const combinedStrings = combine('Tiago', 'Jimmy')
+const combinedNumbers2 = combine('2', '3', 'as-number')
+console.log(combinedNumbers2)
+
+const combinedStrings = combine('Tiago', 'Jimmy', 'as-text')
 console.log(combinedStrings)
