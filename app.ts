e01da53e0a36195dcba07159cd38a236b297443f
Tiago Gomes <tidgomes@gmail.com>
2023-04-18 08:43:08 -0300

    enums in typescript

--- app.ts
+++ app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+enum Roletype {
+  ADMIN,
+  USER,
+  AUTHOR
+}
+
+const test = {
+  name: 'Tiago',
+  age: 28,
+  role: Roletype.ADMIN
+}
+
+if (test.role === Roletype.ADMIN) console.log(test.role)
